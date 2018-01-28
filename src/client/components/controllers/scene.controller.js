bca07c8f23711923eda51b1574f6fcb49a535cb5
Cyrille Gindreau <cyrillegin@gmail.com>
2018-01-28 13:26:57 -0700

    adding controllers, filling out scene init

---
+++ src/client/components/controllers/scene.controller.js
@@ -0,0 +1,58 @@
+/*
+    Scene.controller.js
+    Authors: Cyrille Gindreau
+
+    initScene()
+    initializes the scene and renderer of THREE scene.
+
+    render()
+    renders the next frame of the scene.
+
+*/
+import 'three';
+
+function initScene(container) {
+    console.log('building scene');
+    const app = {
+        scene: new THREE.Scene(),
+        camera: null,
+        cameraControls: null,
+        lights: {},
+        meshes: {},
+        renderer: null,
+        container,
+        loaded: false,
+    };
+    initRenderer(app);
+    return app;
+}
+
+function initRenderer(app) {
+    app.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true,
+    });
+    app.renderer.shadowMap.enabled = true;
+    app.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    app.renderer.gammaInput = true;
+    app.renderer.gammaOutput = true;
+    app.renderer.autoClear = false;
+    app.renderer.sortObjects = false;
+    app.renderer.setSize(app.container.offsetWidth, app.container.offsetHeight);
+
+    app.renderer.setClearColor(0xffffff, 0);
+
+    app.container.appendChild(app.renderer.domElement);
+    $(app.renderer.domElement).css('position', 'absolute');
+
+    app.render = () => {
+        if (app.loaded = false) {
+            return;
+        }
+        console.log('render.');
+        requestAnimationFrame(app.render);
+    };
+    return app;
+}
+
+export default initScene;
