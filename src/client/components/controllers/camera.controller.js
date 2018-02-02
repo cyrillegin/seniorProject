b22da841883ab84fa7fed11932121acd043864b4
Cyrille Gindreau <cyrillegin@gmail.com>
2018-02-01 18:45:47 -0700

    limming down function

--- src/client/components/controllers/camera.controller.js
+++ src/client/components/controllers/camera.controller.js
@@ -11,7 +11,7 @@ import 'three/examples/js/controls/OrbitControls';
 
 function initCamera(app) {
     app.camera = new THREE.PerspectiveCamera(45, app.container.offsetWidth / app.container.offsetHeight, 0.1, 80000);
-    app.camera.position.set(10, 0, 0);
+    app.camera.position.set(0, 1000, 1000);
     app.camera.lookAt(app.scene.position);
     app.cameraControls = new THREE.OrbitControls(app.camera, app.renderer.domElement);
     return app;
--- src/client/components/controllers/curves.controller.js
+++ src/client/components/controllers/curves.controller.js
@@ -1,96 +1,62 @@
-let container, stats;
-let camera, scene, renderer;
-let splineHelperObjects = [], splineOutline;
+const splineHelperObjects = [];
 let positions = [];
-let options;
 const geometry = new THREE.BoxGeometry(20, 20, 20);
-let transformControl;
 let splinePointsLength = 4;
 const ARC_SEGMENTS = 200;
 let splineMesh;
 const splines = {};
-const params = {
-    uniform: true,
-    tension: 0.5,
-    centripetal: true,
-    chordal: true,
-    addPoint,
-};
+
+const curves = [{
+    label: 'AF',
+    points: [[20, 0, 0], [0, 0, 50], [25, 0, 100]],
+}];
 
 function initCurves(app) {
 
+    buildCurve(app);
+    return app;
+}
 
-    const planeGeometry = new THREE.PlaneGeometry(2000, 2000);
-    planeGeometry.rotateX(- Math.PI / 2);
+function buildCurve(app) {
 
-    const planeMaterial = new THREE.ShadowMaterial({opacity: 0.2});
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-    plane.position.y = -200;
-    plane.receiveShadow = true;
-    app.scene.add(plane);
-
-    const helper = new THREE.GridHelper(2000, 100);
-    helper.position.y = - 199;
-    helper.material.opacity = 0.25;
-    helper.material.transparent = true;
-    app.scene.add(helper);
-    // var axes = new THREE.AxesHelper( 1000 );
-    // axes.position.set( - 500, - 500, - 500 );
-    // scene.add( axes );
-
-    /** *****
-* Curves
-******** */
-    for (var i = 0; i < splinePointsLength; i ++) {
+    for (let i = 0; i < splinePointsLength; i ++) {
         addSplineObject(app, positions[ i ]);
     }
+
     positions = [];
-    for (var i = 0; i < splinePointsLength; i ++) {
+    for (let i = 0; i < splinePointsLength; i ++) {
         positions.push(splineHelperObjects[ i ].position);
     }
+
     const geometry = new THREE.Geometry();
-    for (var i = 0; i < ARC_SEGMENTS; i ++) {
+    for (let i = 0; i < ARC_SEGMENTS; i ++) {
         geometry.vertices.push(new THREE.Vector3());
     }
-    let curve = new THREE.CatmullRomCurve3(positions);
+    // Create a curve
+    const curve = new THREE.CatmullRomCurve3(positions);
     curve.curveType = 'catmullrom';
-    curve.mesh = new THREE.Line(geometry.clone(), new THREE.LineBasicMaterial({
-        color: 0xff0000,
-        opacity: 0.35,
-        linewidth: 2,
-    }));
-    curve.mesh.castShadow = true;
-    splines.uniform = curve;
-    curve = new THREE.CatmullRomCurve3(positions);
-    curve.curveType = 'centripetal';
-    curve.mesh = new THREE.Line(geometry.clone(), new THREE.LineBasicMaterial({
-        color: 0x00ff00,
-        opacity: 0.35,
-        linewidth: 2,
-    }));
-    curve.mesh.castShadow = true;
-    splines.centripetal = curve;
-    curve = new THREE.CatmullRomCurve3(positions);
-    curve.curveType = 'chordal';
     curve.mesh = new THREE.Line(geometry.clone(), new THREE.LineBasicMaterial({
         color: 0x0000ff,
         opacity: 0.35,
         linewidth: 2,
     }));
     curve.mesh.castShadow = true;
+    splines.uniform = curve;
+
+    curve.mesh.castShadow = true;
+
     splines.chordal = curve;
     for (const k in splines) {
         const spline = splines[ k ];
         app.scene.add(spline.mesh);
     }
-    load([new THREE.Vector3(289.76843686945404, 452.51481137238443, 56.10018915737797),
-        new THREE.Vector3(-53.56300074753207, 171.49711742836848, -14.495472686253045),
-        new THREE.Vector3(-91.40118730204415, 176.4306956436485, -6.958271935582161),
-        new THREE.Vector3(-383.785318791128, 491.1365363371675, 47.869296953772746)]);
-        
-        return app
+    load([new THREE.Vector3(200, 460, 0),
+        new THREE.Vector3(70, 170, 0),
+        new THREE.Vector3(-70, 170, 0),
+        new THREE.Vector3(-200, 460, 0)]);
+    return app;
 }
+
 function addSplineObject(app, position) {
     const material = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
     const object = new THREE.Mesh(geometry, material);
@@ -108,18 +74,19 @@ function addSplineObject(app, position) {
     return object;
 }
 
-function load(new_positions) {
-    while (new_positions.length > positions.length) {
+function load(newPositions) {
+    while (newPositions.length > positions.length) {
         addPoint();
     }
-    while (new_positions.length < positions.length) {
+    while (newPositions.length < positions.length) {
         removePoint();
     }
     for (let i = 0; i < positions.length; i ++) {
-        positions[ i ].copy(new_positions[ i ]);
+        positions[ i ].copy(newPositions[ i ]);
     }
     updateSplineOutline();
 }
+
 function addPoint() {
     splinePointsLength ++;
     positions.push(addSplineObject().position);
@@ -135,7 +102,6 @@ function removePoint() {
     scene.remove(splineHelperObjects.pop());
     updateSplineOutline();
 }
-
 
 function updateSplineOutline() {
     for (const k in splines) {
