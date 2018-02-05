8ffa5a18d4a941fe033ec643df3233f3203945ba
Cyrille Gindreau <cyrillegin@gmail.com>
2018-02-05 13:09:22 -0700

    draws a basic side panale of a boat

--- src/client/components/blueprintEditor/blueprint.controller.js
+++ src/client/components/blueprintEditor/blueprint.controller.js
@@ -6,9 +6,10 @@ import './blueprint.style.scss';
 
 
 const sidePanel = [
-    {x: 1, y: 5}, {x: 20, y: 20},
-    {x: 40, y: 10}, {x: 60, y: 40},
-    {x: 80, y: 5}, {x: 100, y: 60},
+    {x: 1, y: 1}, {x: 500, y: 1},
+    {x: 500, y: 1}, {x: 500, y: 20},
+    {x: 250, y: 150}, {x: 1, y: 170},
+    {x: 1, y: 170}, {x: 1, y: 1},
 ];
 
 class BlueprintEditor extends HTMLElement {
@@ -20,20 +21,20 @@ class BlueprintEditor extends HTMLElement {
     createdCallback() {
         this.innerHTML = template;
         const elem = $('#blueprint-container')[0];
-        this.svgContainer = d3.select('blueprint-container')
+        this.svgContainer = d3.select('#blueprint-container')
             .append('svg')
             .attr('width', elem.clientWidth)
             .attr('height', '600');
 
         const lineFunction = d3.line()
-            .x(function(d) { return d.x; })
-            .y(function(d) { return d.y; })
+            .x((d) => d.x)
+            .y((d) => d.y)
             .curve(d3.curveBasis);
-        const graph = this.svgContainer.append('path')
+        this.svgContainer.append('path')
             .attr('d', lineFunction(sidePanel))
             .attr('stroke', 'blue')
             .attr('stroke-width', 2)
-            .attr('fill', 'none')
+            .attr('fill', 'none');
 
     }
 }
