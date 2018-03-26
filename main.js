74ec75360211643c0eb554418b20036963f2b8e2
Alec Gonzales <baldwin5513@gmail.com>
2018-03-26 17:05:05 -0600

    Linted

--- main.js
+++ main.js
@@ -1,48 +1,55 @@
-const {app, BrowserWindow, Menu} = require('electron');
-//const path = require('path');
-//const url = require('url');
-const electron = require('electron')
+import {
+    app,
+    BrowserWindow,
+    // Menu
+} from 'electron';  // eslint-disable-line
+// const path = require('path');
+// const url = require('url');
+// const electron = require('electron')
 
 let win = null;
 
 
 function createWindow() {
-  // Initialize the window to our specified dimensions
-  win = new BrowserWindow({width: 1000, height: 600});
-  // Specify entry point
-  win.loadURL('http://localhost:3000');
-  
+    // Initialize the window to our specified dimensions
+    win = new BrowserWindow({
+        width: 1000,
+        height: 600,
+    });
+    // Specify entry point
+    win.loadURL('http://localhost:3000');
 
-  //build menu from template
-  /*const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    Menu.setApplicationMenu(mainMenu);*/
 
-// Show dev tools
-  win.webContents.openDevTools();
-  // Remove window once app is closed
-  win.on('closed', function () {
-    win = null;
-  });
+    // build menu from template
+    /*  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+      Menu.setApplicationMenu(mainMenu); */
+
+    // Show dev tools
+    win.webContents.openDevTools();
+    // Remove window once app is closed
+    win.on('closed', () => {
+        win = null;
+    });
 }
 
-app.on('ready', function () {
-  createWindow();
+app.on('ready', () => {
+    createWindow();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
-/*app.on('ready', function (){
+/* app.on('ready', function (){
     //Create browser window
     win = new BrowserWindow({width:800, height:600, icon:__dirname+'/img/Boat1.png'});
 
@@ -59,11 +66,11 @@ app.on('activate', function () {
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
       Menu.setApplicationMenu(mainMenu);
 
-});*/
+}); */
 
-
-//create menu template
-/*const mainMenuTemplate = [
+// NOTE: imsgsdggj
+// create menu template
+/* const mainMenuTemplate = [
   {  label: 'File',
      submenu:[{label: 'New Project'},
               {label: 'Open Project',
@@ -99,4 +106,4 @@ app.on('activate', function () {
              }]
   }
 
-];*/
+]; */
