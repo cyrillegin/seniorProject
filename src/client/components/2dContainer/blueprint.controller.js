b90348857774bf0c14b24b3a2dc9a23bb8cfafdb
Tim Kraus <tkraus666@gmail.com>
2018-03-31 18:06:31 -0600

    Bottom panel, bow and stern panels, and interior frame added

--- src/client/components/2dContainer/blueprint.controller.js
+++ src/client/components/2dContainer/blueprint.controller.js
@@ -1,6 +1,6 @@
 // Global imports
 import * as d3 from 'd3';
-/* Original sidepanl coordinates
+/* Original sidepanel coordinates
 const sidePanel = [
     {x: 1, y: 1}, {x: 500, y: 1},
     {x: 500, y: 1}, {x: 500, y: 20},
@@ -20,12 +20,59 @@ const aftForeChine = [
     {x: 55, y: 16}, {x: 61, y: 15},
 ];
 
-const bow = [
+const bowCon = [
     {x: 10, y: 10}, {x: 14, y: 15},
 ];
 
-const stern = [
+const sternCon = [
     {x: 60, y: 10}, {x: 61, y: 15},
+];
+
+const aftForeChine2 = [
+    {x: 10, y: 26.5}, {x: 15, y: 24},
+    {x: 23, y: 22.5}, {x: 39, y: 22.5},
+    {x: 52, y: 24}, {x: 57, y: 26},
+];
+
+const lowBow = [
+    {x: 10, y: 26.5}, {x: 10, y: 28},
+];
+
+const lowStern = [
+    {x: 57, y: 26}, {x: 57, y: 28},
+];
+
+const keel = [
+    {x: 10, y: 28}, {x: 57, y: 28},
+];
+
+const stern = [
+    {x: 10, y: 33}, {x: 20, y: 33},
+    {x: 18, y: 39}, {x: 12, y: 39},
+    {x: 10, y: 33},
+];
+
+const bow = [
+    {x: 25, y: 33}, {x: 31, y: 33},
+    {x: 30, y: 39}, {x: 26, y: 39},
+    {x: 25, y: 33},
+];
+
+const frame0 = [
+    {x: 36, y: 33}, {x: 38, y: 39},
+    {x: 52, y: 39}, {x: 54, y: 33},
+];
+
+const box0 = [
+    {x: 10, y: 7.5}, {x: 61, y: 7.5},
+    {x: 61, y: 17.5}, {x: 10, y: 17.5},
+    {x: 10, y: 7.5},
+];
+
+const box1 = [
+    {x: 10, y: 22.5}, {x: 57, y: 22.5},
+    {x: 57, y: 28}, {x: 10, y: 28},
+    {x: 10, y: 22.5},
 ];
 
 export default class BlueprintEditor {
@@ -62,6 +109,18 @@ export default class BlueprintEditor {
             .y((d) => (d.y) * elem.clientWidth * scale)
             .curve(d3.curveBasis);
 
+        // Creae dimensional boxes around panels
+        this.canvas.append('path')
+            .attr('d', lineFunction(box0))
+            .attr('stroke', 'blue')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+        this.canvas.append('path')
+            .attr('d', lineFunction(box1))
+            .attr('stroke', 'blue')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+
         // Create the fore and aft Beam curves (simplified for now)
         this.canvas.append('path')
             .attr('d', lineFunc(aftForeBeam))
@@ -78,12 +137,59 @@ export default class BlueprintEditor {
 
         // Connect the Beam and Chine curves
         this.canvas.append('path')
-	    .attr('d', lineFunction(bow))
+	          .attr('d', lineFunction(bowCon))
             .attr('stroke', 'red')
             .attr('stroke-width', 2)
             .attr('fill', 'none');
         this.canvas.append('path')
+            .attr('d', lineFunction(sternCon))
+            .attr('stroke', 'red')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+
+        // Create aft fore chine curve for bottom panel
+        this.canvas.append('path')
+            .attr('d', lineFunc(aftForeChine2))
+            .attr('stroke', 'red')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+
+        // Connect bow and stern points
+        this.canvas.append('path')
+            .attr('d', lineFunction(lowBow))
+            .attr('stroke', 'red')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+        this.canvas.append('path')
+            .attr('d', lineFunction(lowStern))
+            .attr('stroke', 'red')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+
+        // Create keel line
+        this.canvas.append('path')
+            .attr('d', lineFunction(keel))
+            .attr('stroke', 'red')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+
+        // Create Stern panel
+        this.canvas.append('path')
             .attr('d', lineFunction(stern))
+            .attr('stroke', 'red')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+
+        // Create bow panel
+        this.canvas.append('path')
+            .attr('d', lineFunction(bow))
+            .attr('stroke', 'red')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
+
+        // Create inner frame
+        this.canvas.append('path')
+            .attr('d', lineFunction(frame0))
             .attr('stroke', 'red')
             .attr('stroke-width', 2)
             .attr('fill', 'none');
