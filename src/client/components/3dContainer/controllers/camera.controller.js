f948749a87a06c7a059926732a66b8d8fdb43c82
Cyrille Gindreau <cyrillegin@gmail.com>
2018-04-11 16:54:08 -0600

    adding camera control toolbar

--- src/client/components/3dContainer/controllers/camera.controller.js
+++ src/client/components/3dContainer/controllers/camera.controller.js
@@ -10,11 +10,22 @@ import 'three';
 import 'three/examples/js/controls/OrbitControls';
 
 export default class CameraController {
+    constructor() {
+        'ngInject';
+
+        this.camera = null;
+    }
     initCamera(app) {
         app.camera = new THREE.PerspectiveCamera(45, app.container.offsetWidth / app.container.offsetHeight, 0.1, 80000);
         app.camera.position.set(-100, 50, -100);
-        app.cameraControls = new THREE.OrbitControls(app.camera, app.renderer.domElement);
-
+        app.camera.cameraControls = new THREE.OrbitControls(app.camera, app.renderer.domElement);
+        this.initCameraMenu(app.camera);
         return app;
+    }
+
+    initCameraMenu(app) {
+        document.querySelector('#camera-front-button').addEventListener('click', (e) => {
+            app.camera.position.set(0, 0, 0)
+        });
     }
 }
