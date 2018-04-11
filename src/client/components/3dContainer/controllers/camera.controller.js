e3f8e65c3e59f2e0cf1c47ceda58d1b4941a3126
Cyrille Gindreau <cyrillegin@gmail.com>
2018-04-11 16:04:19 -0600

    turns camera controller into a class

--- src/client/components/3dContainer/controllers/camera.controller.js
+++ src/client/components/3dContainer/controllers/camera.controller.js
@@ -9,11 +9,12 @@
 import 'three';
 import 'three/examples/js/controls/OrbitControls';
 
-function initCamera(app) {
-    app.camera = new THREE.PerspectiveCamera(45, app.container.offsetWidth / app.container.offsetHeight, 0.1, 80000);
-    app.camera.position.set(-100, 50, -100);
-    app.cameraControls = new THREE.OrbitControls(app.camera, app.renderer.domElement);
-    return app;
-}
+export default class CameraController {
+    initCamera(app) {
+        app.camera = new THREE.PerspectiveCamera(45, app.container.offsetWidth / app.container.offsetHeight, 0.1, 80000);
+        app.camera.position.set(-100, 50, -100);
+        app.cameraControls = new THREE.OrbitControls(app.camera, app.renderer.domElement);
 
-export default initCamera;
+        return app;
+    }
+}
