8f18aa23982ab9a4a2890cfa3543d05329400e42
Cyrille Gindreau <cyrillegin@gmail.com>
2018-04-29 13:55:28 -0600

    clean

--- tests/utility.test.js
+++ tests/utility.test.js
@@ -107,35 +107,3 @@ describe('mirror.js', () => {
     });
 
 });
-    // const mesh = {
-    //     main: {
-    //         children: [{
-    //             scale: {
-    //                 x: 0,
-    //                 y: 0,
-    //                 z: 0,
-    //             },
-    //         }],
-    //     },
-    // };
-    // let manipulator = null;
-    //
-    // beforeEach(() => {
-    //     manipulator = new Manipulate(mesh);
-    // });
-
-    // it('Adjust the width of the mesh.', () => {
-    //     manipulator.adjustWidth(100);
-    //     assert(mesh.main.children[0].scale.z === 1.5);
-    // });
-    //
-    // it('Adjust the height of the mesh.', () => {
-    //     manipulator.adjustHeight(100);
-    //     assert(mesh.main.children[0].scale.y === 1.5);
-    // });
-    //
-    // it('Adjust the length of the mesh.', () => {
-    //     manipulator.adjustLength(100);
-    //     assert(mesh.main.children[0].scale.x === 1.5);
-    // });
-//});
